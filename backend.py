dd061a4d6aab3f43d90064913032a59830f67224
Mayukh Baruah <mayukhbaruah91@gmail.com>
2025-06-06 13:27:05 +0530

    1st commit developer brunch

---
+++ backend.py
@@ -0,0 +1,12 @@
+# check prime number 
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+num = int(input("Enter a number to check if it's prime: "))     
+if is_prime(num):
+    print(f"{num} is a prime number.")
