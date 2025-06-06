41a6ce2b6c66880bc21df0ff6b816621fe60836f
Mayukh Baruah <mayukhbaruah91@gmail.com>
2025-06-06 13:01:14 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,13 @@
+try:
+    num = int(input("Enter a number: "))
+    result = 18/num
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+except ValueError:
+    print("Error: Invalid input. Please enter a valid number.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+else:
+    print(f"The result is: {result}")
+finally:
+    print("you are good to go .")
